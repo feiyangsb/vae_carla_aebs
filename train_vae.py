3d37325f94c6f3bf9ed213780fef94fbd9486a97
Feiyang Cai <feiyang.cai@vanderbilt.edu>
2020-02-21 22:39:22 +0800

    add the author info

--- train_vae.py
+++ train_vae.py
@@ -1,3 +1,11 @@
+"""
+@author Feiyang Cai
+@email feiyang.cai@vanderbilt.edu
+@create date 2020-02-21 22:38:53
+@modify date 2020-02-21 22:38:53
+@desc main code to train the vae
+"""
+
 from scripts.vae_trainer import VAETrainer
 
 data_path = "./data/training/"
